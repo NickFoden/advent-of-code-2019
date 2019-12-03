c4b9c0d09280ef73b527f58cf9db97667b636a9d
Nick Foden <nicfoden@gmail.com>
2019-12-03 10:40:40 -0500

    Set up day 3 tests

---
+++ Day3/TestData/test1.js
@@ -0,0 +1,25 @@
+const TEST_ONE_WIRE_ONE = [
+  "R75",
+  "D30",
+  "R83",
+  "U83",
+  "L12",
+  "D49",
+  "R71",
+  "U7",
+  "L72"
+];
+
+const TEST_ONE_WIRE_TWO = [
+  "U62",
+  "R66",
+  "U55",
+  "R34",
+  "D71",
+  "R55",
+  "D58",
+  "R83"
+];
+
+module.exports.test1Wire1 = TEST_ONE_WIRE_ONE;
+module.exports.test1Wire2 = TEST_ONE_WIRE_TWO;
---
+++ Day3/day3.js
@@ -0,0 +1,7 @@
+const manhatDist = (course1, cours2) => {
+  let cross = 159;
+
+  return cross;
+};
+
+module.exports = manhatDist

bd962ecbf80095343963b25286f2c6209243151d
Nick Foden <nicfoden@gmail.com>
2019-12-04 00:39:50 -0500

    Day 4 part 1

--- Day4/day4.js
+++ Day4/day4.js
@@ -1,5 +1,43 @@
-const day4 = () => {
-  return 0;
+const START = 145852;
+const END = 616942;
+
+const secureContainer = () => {
+  const range = END - START;
+  let floor = 145852;
+  let count = 0;
+
+  for (let i = 0; i < range; i++) {
+    isLegit((floor += 1)) ? (count += 1) : null;
+  }
+
+  return count;
 };
 
-module.exports = day4;
+const isLegit = val => {
+  let double = false;
+  if (val.toString().length > 6) {
+    return false;
+  }
+  if (val < START) {
+    return false;
+  }
+  if (val > END) {
+    return false;
+  }
+  let valArray = val.toString().split("");
+  for (let i = 0; i < valArray.length - 1; i++) {
+    if (valArray[i] === valArray[i + 1]) {
+      double = true;
+    }
+    if (valArray[i] > valArray[i + 1]) {
+      return false;
+    }
+  }
+  if (double) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = secureContainer;
--- Day4/day4.spec.js
+++ Day4/day4.spec.js
@@ -1,13 +1,7 @@
-const day4 = require("./day4");
+const secureContainer = require("./day4");
 
 describe("Day 4 part 1", () => {
   test("First Test", () => {
-    expect(day4()).toEqual(0);
-  });
-  test("Second Test", () => {
-    expect(day4()).toEqual(0);
-  });
-  test("Solve part 1", () => {
-    expect(day4()).toEqual(0);
+    expect(secureContainer()).toEqual(1767);
   });
 });

e4439530a11b4ade71a1c1cbab73ee36ee4839ba
Nick Foden <nicfoden@gmail.com>
2019-12-05 15:05:42 -0500

    Day 5 part 1 and clean up

--- Day5/day5.js
+++ Day5/day5.js
@@ -1,5 +1,37 @@
-const day5 = () => {
-  return 0;
+const day5 = (intsArray, userInput = 1) => {
+  let resultArray = [...intsArray];
+  let result = "0";
+
+  const getMemory = (mode, index) => {
+    return mode === "0" ? resultArray[resultArray[index]] : resultArray[index];
+  };
+
+  for (let i = 0; i < resultArray.length; ) {
+    const current = resultArray[i].padStart(5, "0");
+    const oCode = Number(current.slice(3));
+    const mode1 = current.slice(2, 3);
+    const mode2 = current.slice(1, 2);
+    // const mode3 = current.slice(0, 1);
+    if (oCode === 99) {
+      break;
+    } else if (oCode === 1) {
+      resultArray[resultArray[i + 3]] = `${Number(getMemory(mode1, i + 1)) +
+        Number(getMemory(mode2, i + 2))}`;
+      i += 4;
+    } else if (oCode === 2) {
+      resultArray[resultArray[i + 3]] = `${getMemory(mode1, i + 1) *
+        getMemory(mode2, i + 2)}`;
+
+      i += 4;
+    } else if (oCode === 3) {
+      resultArray[resultArray[i + 1]] = `${userInput}`;
+      i += 2;
+    } else if (oCode === 4) {
+      result = getMemory(mode1, i + 1);
+      i += 2;
+    }
+  }
+  return result;
 };
 
 module.exports = day5;
--- Day5/day5.spec.js
+++ Day5/day5.spec.js
@@ -1,7 +1,11 @@
 const day5 = require("./day5");
+const DATA = require("./input");
 
 describe("Day 5", () => {
-  test("First test", () => {
-    expect(day5()).toEqual(0);
+  test("test example with input 2", () => {
+    expect(day5(["3", "0", "4", "0", "99"], 2)).toEqual("2");
+  });
+  xtest("Final test", () => {
+    expect(day5(DATA)).toEqual("answer");
   });
 });

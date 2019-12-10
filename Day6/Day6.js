5f21cdaf98f4002cb30d2ce71ba8f4bd0aa8d149
Nick Foden <nicfoden@gmail.com>
2019-12-10 10:00:02 -0500

    Day 6 part 1

--- Day6/Day6.js
+++ Day6/Day6.js
@@ -1,5 +1,26 @@
-const day6 = () => {
-  return 0;
+const day6 = data => {
+  let result = {};
+  let orbits = 0;
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i].split(")");
+    const [one, two] = item;
+    result[two] = one;
+  }
+
+  let trailerKeys = Object.keys(result);
+  // let trailerArray = Object.values(result);
+  for (let i = 0; i < trailerKeys.length; i++) {
+    const current = trailerKeys[i];
+
+    let next = result[current];
+
+    while (next) {
+      next = result[next];
+      orbits += 1;
+    }
+  }
+
+  return orbits;
 };
 
 module.exports = day6;
